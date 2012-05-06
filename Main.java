bfb15b5191d94b954bde534ec8bf26cf06dc6bbd
diogo.raphael.cravo <diogo.raphael.cravo@803a506b-e83e-3d46-6453-efd46dbf5eeb>
2012-05-06 22:07:46 +0000

    Exemplo de uso de jwlgl. Início da implementação.

---
+++ Main.java
@@ -0,0 +1,15 @@
+package mlptd;
+
+
+/**
+ * @author jediTofu
+ * @see <a href="http://lwjgl.org/">LWJGL Home Page</a>
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    td towerDefense = new td();
+
+  }
+
+}
