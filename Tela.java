366214c69ec649ce112999b14d669876b01a27fc
diogo.raphael.cravo <diogo.raphael.cravo@803a506b-e83e-3d46-6453-efd46dbf5eeb>
2012-05-09 11:38:24 +0000

    ... esqueci de adicionar o arquivo.

---
+++ Tela.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mlptd;
+
+/**
+ *
+ * @author drcravo
+ */
+public class Tela {
+    public static int WIDTH = 640;
+    public static int HEIGHT = 480;
+
+}
