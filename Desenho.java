cec11ed078e05c7556977754a650476fce83f911
diogo.raphael.cravo <diogo.raphael.cravo@803a506b-e83e-3d46-6453-efd46dbf5eeb>
2012-05-09 15:27:38 +0000

    Caminho de linhas alternadas pronto. Inimigos são colocados no centro das Tiles Passadouro. Criados novos construtores de cópia. Classe Camera pronta para camera ortográfica (provavelmente a única que será implementada).

--- Desenho.java
+++ Desenho.java
@@ -1,6 +1,7 @@
 package mlptd;
 
 import org.lwjgl.util.Color;
+import static org.lwjgl.opengl.GL11.*;
 
 /**
  * Um desenho qualquer.
@@ -22,24 +23,19 @@ public class Desenho {
      * @param _tamanhoEmPorcentagem Usado para resize.
      */
     public Desenho(float _posX, float _posY, float _comprimento, float _largura, int _tamanhoEmPorcentagem){
-        cor = new Color(Color.GREEN);
+        cor = new Color(Color.WHITE);
         posX = _posX;
         posY = _posY;
         comprimento = _comprimento;
-        largura = _largura;tamanhoEmPorcentagem = _tamanhoEmPorcentagem;
+        largura = _largura;
+        tamanhoEmPorcentagem = _tamanhoEmPorcentagem;
         altura = 1;
         redimensionar(1);
     }
     public Desenho(float _posX, float _posY, float _comprimento, float _largura, int _tamanhoEmPorcentagem, float _altura){
-        cor = new Color(Color.GREEN);
-        posX = _posX;
-        posY = _posY;
-        comprimento = _comprimento;
-        largura = _largura;tamanhoEmPorcentagem = _tamanhoEmPorcentagem;
+        this(_posX, _posY, _comprimento, _largura, _tamanhoEmPorcentagem);
         altura = _altura;
-        redimensionar(1);
     }
-
 
     public float getPosX(){
         return posX;
@@ -69,13 +65,14 @@ public class Desenho {
     }
 
     /**
+     * O movimento é feito de uma só vez.
      * @param _destino Ponto para onde o desenho deve ir.
      */
     public void mover(float _posX, float _posY){
         posX = _posX;
         posY = _posY;
     }
-
+    
     /**
      * @param _tamanhoEmPorcentagem O tamanho em rela��o ao tamanho inicial (de cria��o) do desenho.
      */
@@ -84,8 +81,23 @@ public class Desenho {
     }
 	
     /**
-     * Desenha na tela com base em seu tamanho e posi��o.
-     * Deve ser implementada em casa classe descendente.
+     * Desenha na tela com base em seu tamanho e posicao.
+     * O objeto desenhado é um retângulo.
      */
-    public void desenhar(){}
+    public void desenhar(){
+        glPushMatrix();
+        glTranslatef(posX,posY,0.0f);
+        glRotatef(0,0.0f,0.0f,1.0f);
+        glTranslatef(-(100 >> 1),-(100 >> 1),0.0f);
+        glColor3f((float) (cor.getRed()/255.0),
+                  (float) (cor.getGreen()/255.0),
+                  (float) (cor.getBlue()/255.0));
+        glBegin(GL_QUADS);
+            glTexCoord2f(0.0f,0.0f); glVertex2f(0.0f,0.0f);
+            glTexCoord2f(1.0f,0.0f); glVertex2f(tamanhoEmPorcentagem*comprimento, 0.0f);
+            glTexCoord2f(1.0f,1.0f); glVertex2f(tamanhoEmPorcentagem*comprimento, tamanhoEmPorcentagem*largura);
+            glTexCoord2f(0.0f,1.0f); glVertex2f(0.0f, tamanhoEmPorcentagem*largura);
+        glEnd();
+        glPopMatrix();
+    }
 }
