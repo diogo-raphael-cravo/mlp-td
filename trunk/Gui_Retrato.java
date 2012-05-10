4d1a9552c4528cb2a7627424829442cbd0ab4585
diogo.raphael.cravo <diogo.raphael.cravo@803a506b-e83e-3d46-6453-efd46dbf5eeb>
2012-05-10 16:34:12 +0000

    ... esqueci de adicionar os arquivos novos.

---
+++ trunk/Gui_Retrato.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mlptd;
+
+import org.lwjgl.util.Color;
+
+/**
+ * Elemento da gui que é capaz de conter um retrato de algum desenho.
+ * Utilizado em Gui_BarraInferior para mostrar o elemento que está selecionado.
+ * @author drcravo
+ */
+public class Gui_Retrato extends Desenho {
+
+    /**
+     * Desenho mostrado por este retrato.
+     */
+    private Desenho desenhoExibido;
+
+    /**
+     * @param _posX, _posY A posi��o do ponto superior esquerdo na tela.
+     * @param _comprimento, _largura, _altura Comprimento, largura e altura.
+     * @param _tamanhoEmPorcentagem Usado para resize.
+     */
+    public Gui_Retrato(float _posX, float _posY, float _comprimento, float _largura,
+            int _tamanhoEmPorcentagem){
+        super(_posX, _posY, _comprimento, _largura, _tamanhoEmPorcentagem);
+        cor = new Color(Color.BLACK);
+        desenhoExibido = null;
+    }
+
+    /**
+     * Exibe este desenho neste retrato.
+     * Se houver desenho exibido, retira-o e desenha o de parâmetro.
+     * @param _desenho Desenho a ser exibido.
+     */
+    public void exibir(Desenho _desenho){
+        desenhoExibido = _desenho;
+    }
+
+    /**
+     * Limpa o desenho exibido.
+     */
+    public void retirarDesenhoExibido(){
+        desenhoExibido = null;
+    }
+
+    /**
+     * Desenha na tela com base em seu tamanho e posicao.
+     * O objeto desenhado é um retângulo.
+     */
+    public void desenhar(){
+        super.desenhar();
+        if(desenhoExibido != null){
+            desenhoExibido.desenhar();
+        }
+    }
+    
+}
