7b44949251106a3d4a476f87512ad5fc73cd684a
diogo.raphael.cravo <diogo.raphael.cravo@803a506b-e83e-3d46-6453-efd46dbf5eeb>
2012-05-10 16:34:12 +0000

    ... esqueci de adicionar os arquivos novos.

---
+++ ControladorJogo.java
@@ -0,0 +1,56 @@
+package mlptd;
+
+import java.util.Vector;
+import org.lwjgl.input.Mouse;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Controla eventos que pertençam a um jogo.
+ * @author drcravo
+ */
+public class ControladorJogo
+    implements MouseListener {
+
+    /**
+     * O jogo do controlador.
+     */
+     Jogo jogo;
+
+    /**
+     * O desenho que foi selecionado pelo mouse.
+     */
+     Desenho desenhoSelecionado;
+
+    public ControladorJogo(Jogo _jogo){
+        jogo = _jogo;
+        desenhoSelecionado = null;
+    }
+
+    public void houveMouseDown() {
+        boolean selecionouDesenho = false;
+        float mouseX = Tela.xTelaParaGlobal(Mouse.getX());
+        float mouseY = Tela.yTelaParaGlobal(Mouse.getY());
+        Vector<Desenho> todosDesenhosCriados = Desenho.getTodosDesenhosCriados();
+        for(Desenho desenho : todosDesenhosCriados){
+            if(desenho.contem(mouseX+desenho.getComprimento(), mouseY+desenho.getLargura())){
+                if(desenhoSelecionado != null){
+                    desenhoSelecionado.restaurarTransparencia();
+                }
+                desenhoSelecionado = desenho;
+                desenhoSelecionado.tornarTransparente();
+                selecionouDesenho = true;
+                Tela.getTela().getGuiBarraInferior().getGuiRetrato().exibir(desenhoSelecionado);
+            }
+        }
+        if(!selecionouDesenho && desenhoSelecionado != null){
+            desenhoSelecionado.restaurarTransparencia();
+            desenhoSelecionado = null;
+            Tela.getTela().getGuiBarraInferior().getGuiRetrato().retirarDesenhoExibido();
+        }
+    }
+
+}
