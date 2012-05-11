f296dcbadc5d359329107351c7facefb5be5c9e2
diogo.raphael.cravo <diogo.raphael.cravo@803a506b-e83e-3d46-6453-efd46dbf5eeb>
2012-05-11 20:21:14 +0000

    Carregamento de texturas iniciado. Criada função adicionarTextura em Desenho. Adicionada classe Arquivos para conter endereços de arquivos que precisam ser carregados (como texturas). Adicionada biblioteca slick-util.jar para carregamento de texturas. Note que o carregamento ainda não está bom, as texturas ficam distorcidas.

---
+++ Arquivos.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mlptd;
+
+/**
+ * Classe para acesso estático de nomes de arquivos.
+ * @author diogo
+ */
+public class Arquivos {
+    public static String ARQUIVO_TEXTURA_MURO = "C:\\Documents and Settings\\NUTED\\Desktop\\muro.png";
+}
