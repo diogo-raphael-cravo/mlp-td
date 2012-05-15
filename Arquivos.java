d8ba6156d480b1538c7c0c9e6101ad2a5f8a8c57
diogo.raphael.cravo <diogo.raphael.cravo@803a506b-e83e-3d46-6453-efd46dbf5eeb>
2012-05-15 04:55:42 +0000

    Imagens transparentes (usado GIMP do ubuntu).

--- Arquivos.java
+++ Arquivos.java
@@ -12,12 +12,12 @@ package mlp.td;
 public class Arquivos {
     public static final String LINK_BASE = "C:\\Users\\admin\\Desktop\\mlp-td\\";
     public static String ARQUIVO_TEXTURA_MURO = LINK_BASE+"muro2.png";
-    public static String ARQUIVO_TEXTURA_CAVEIRA = LINK_BASE+"caveira4.gif";
-    public static String ARQUIVO_TEXTURA_CAVEIRA_INVERTIDA = LINK_BASE+"caveira42.gif";
+    public static String ARQUIVO_TEXTURA_CAVEIRA = LINK_BASE+"caveira4.png";
+    public static String ARQUIVO_TEXTURA_CAVEIRA_INVERTIDA = LINK_BASE+"caveira42.png";
     public static String ARQUIVO_TEXTURA_GRAMA = LINK_BASE+"grama.jpg";
     public static String ARQUIVO_TEXTURA_LADRILHO = LINK_BASE+"ladrilho.jpg";
-    public static String ARQUIVO_TEXTURA_TORRE = LINK_BASE+"torre.jpg";
-    public static String ARQUIVO_TEXTURA_CANHAO = LINK_BASE+"canhao.jpg";
+    public static String ARQUIVO_TEXTURA_TORRE = LINK_BASE+"torre.png";
+    public static String ARQUIVO_TEXTURA_CANHAO = LINK_BASE+"canhao.png";
     public static String ARQUIVO_TEXTURA_MADEIRA = LINK_BASE+"madeira.jpg";
     public static String ARQUIVO_TEXTURA_X = LINK_BASE+"x.png";
 }
