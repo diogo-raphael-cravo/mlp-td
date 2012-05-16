8128a45f44af658ba10438be793e4e500382e3ab
diogo.raphael.cravo <diogo.raphael.cravo@803a506b-e83e-3d46-6453-efd46dbf5eeb>
2012-05-16 16:38:36 +0000

    Números toscos funcionando.

--- trunk/Texturas.java
+++ trunk/Texturas.java
@@ -41,6 +41,17 @@ public class Texturas {
      public static Texture TIMES_NEW_ROMAN_8;
      public static Texture TIMES_NEW_ROMAN_9;
 
+     public static Texture PIXEL_0;
+     public static Texture PIXEL_1;
+     public static Texture PIXEL_2;
+     public static Texture PIXEL_3;
+     public static Texture PIXEL_4;
+     public static Texture PIXEL_5;
+     public static Texture PIXEL_6;
+     public static Texture PIXEL_7;
+     public static Texture PIXEL_8;
+     public static Texture PIXEL_9;
+
      public static void inicializar(){
          try {
             CAVEIRA = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.ARQUIVO_TEXTURA_CAVEIRA));
@@ -103,8 +114,36 @@ public class Texturas {
          try {
             TIMES_NEW_ROMAN_9 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_TIMES_NEW_ROMAN+"9.PNG"));
          } catch (Exception ex) {}
+
          try {
-            TIMES_NEW_ROMAN_0 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_TIMES_NEW_ROMAN+"0"));
+            PIXEL_0 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"0.png"));
+         } catch (Exception ex) { ex.printStackTrace(); }
+         try {
+            PIXEL_1 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"1.png"));
+         } catch (Exception ex) {}
+         try {
+            PIXEL_2 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"2.png"));
+         } catch (Exception ex) {}
+         try {
+            PIXEL_3 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"3.png"));
+         } catch (Exception ex) {}
+         try {
+            PIXEL_4 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"4.png"));
+         } catch (Exception ex) {}
+         try {
+            PIXEL_5 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"5.png"));
+         } catch (Exception ex) {}
+         try {
+            PIXEL_6 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"6.png"));
+         } catch (Exception ex) {}
+         try {
+            PIXEL_7 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"7.png"));
+         } catch (Exception ex) {}
+         try {
+            PIXEL_8 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"8.png"));
+         } catch (Exception ex) {}
+         try {
+            PIXEL_9 = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream(Arquivos.LINK_PIXEL+"9.png"));
          } catch (Exception ex) {}
      }
 
