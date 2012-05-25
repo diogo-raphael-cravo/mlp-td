6e91078276cdc0f293ab81b5c01bc0d0186f4890
eduardo.g.farias <eduardo.g.farias@803a506b-e83e-3d46-6453-efd46dbf5eeb>
2012-05-25 14:29:45 +0000

    Map utiliza recursão agora.

--- Map.java
+++ Map.java
@@ -1,6 +1,8 @@
 package mlptd;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Map {
 	/**
@@ -11,8 +13,22 @@ public class Map {
 	}
 	
 	public static <E> void map(Collection<E> lista, Method<E> method) {
-		for(E e : lista) {
-			method.method(e);
+		Collection<E> copiaLista = new LinkedList<E>(lista);
+		Map.recursiveMap(copiaLista, method);
+		//for(E e : lista) {
+		//	method.method(e);
+		//}
+	}
+	
+	private static <E> void recursiveMap(Collection<E> lista, Method<E> method) {
+		Iterator<E> iterator = lista.iterator();
+		if(iterator.hasNext()) {
+			E head = iterator.next();
+			method.method(head);
+			iterator.remove();
+			head = null;
+			iterator = null;
+			Map.recursiveMap(lista, method);
 		}
 	}
 }
